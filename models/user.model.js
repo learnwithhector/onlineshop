d4b1b322eb3737d2261173dfc5ed5eded9941bf8
Hector Macafonty <learnwithhector@gmail.com>
2023-01-20 23:44:30 +0000

    add user input validation

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,6 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const db = require('../data/database');
+const db = require("../data/database");
 
 class User {
   constructor(email, password, fullname, street, postal, city) {
@@ -15,13 +15,18 @@ class User {
   }
 
   getUserWithSameEmail() {
-    return db.getDb().collection('users').findOne({ email: this.email });
+    return db.getDb().collection("users").findOne({ email: this.email });
+  }
+
+  async existsAlready() {
+    const existingUser = await this.getUserWithSameEmail();
+    return existingUser ? true : false;
   }
 
   async signup() {
     const hashedPassword = await bcrypt.hash(this.password, 12);
 
-    await db.getDb().collection('users').insertOne({
+    await db.getDb().collection("users").insertOne({
       email: this.email,
       password: hashedPassword,
       name: this.name,
