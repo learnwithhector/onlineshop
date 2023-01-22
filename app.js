40609c1b3e91ab3147eecb90013eb11ce5d8bbf1
Hector Macafonty <learnwithhector@gmail.com>
2023-01-22 01:19:38 +0000

    end of section

--- app.js
+++ app.js
@@ -9,9 +9,16 @@ const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
 const errorHandlerMiddleware = require('./middlewares/error-handler');
 const checkAuthStatusMiddleware = require('./middlewares/check-auth');
+const protectRoutesMiddleware = require('./middlewares/protect-routes');
+const cartMiddleware = require('./middlewares/cart');
+const updateCartPricesMiddleware = require('./middlewares/update-cart-prices');
+const notFoundMiddleware = require('./middlewares/not-found');
 const authRoutes = require('./routes/auth.routes');
 const productsRoutes = require('./routes/products.routes');
 const baseRoutes = require('./routes/base.routes');
+const adminRoutes = require('./routes/admin.routes');
+const cartRoutes = require('./routes/cart.routes');
+const ordersRoutes = require('./routes/orders.routes');
 
 const app = express();
 
@@ -19,12 +26,17 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
+app.use('/products/assets', express.static('product-data'));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const sessionConfig = createSessionConfig();
 
 app.use(expressSession(sessionConfig));
 app.use(csrf());
+
+app.use(cartMiddleware);
+app.use(updateCartPricesMiddleware);
 
 app.use(addCsrfTokenMiddleware);
 app.use(checkAuthStatusMiddleware);
@@ -32,6 +44,11 @@ app.use(checkAuthStatusMiddleware);
 app.use(baseRoutes);
 app.use(authRoutes);
 app.use(productsRoutes);
+app.use('/cart', cartRoutes);
+app.use('/orders', protectRoutesMiddleware, ordersRoutes);
+app.use('/admin', protectRoutesMiddleware, adminRoutes);
+
+app.use(notFoundMiddleware);
 
 app.use(errorHandlerMiddleware);
 
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,30 +1,30 @@
-const User = require("../models/user.model");
-const authUtil = require("../util/authentication");
-const validation = require("../util/validation");
-const sessionFlash = require("../util/session-flash");
+const User = require('../models/user.model');
+const authUtil = require('../util/authentication');
+const validation = require('../util/validation');
+const sessionFlash = require('../util/session-flash');
 
 function getSignup(req, res) {
   let sessionData = sessionFlash.getSessionData(req);
 
   if (!sessionData) {
     sessionData = {
-      email: "",
-      confirmEmail: "",
-      password: "",
-      fullname: "",
-      street: "",
-      postal: "",
-      city: "",
+      email: '',
+      confirmEmail: '',
+      password: '',
+      fullname: '',
+      street: '',
+      postal: '',
+      city: '',
     };
   }
 
-  res.render("customer/auth/signup", { inputData: sessionData });
+  res.render('customer/auth/signup', { inputData: sessionData });
 }
 
 async function signup(req, res, next) {
   const enteredData = {
     email: req.body.email,
-    confirmEmail: req.body["confirm-email"],
+    confirmEmail: req.body['confirm-email'],
     password: req.body.password,
     fullname: req.body.fullname,
     street: req.body.street,
@@ -41,17 +41,17 @@ async function signup(req, res, next) {
       req.body.postal,
       req.body.city
     ) ||
-    !validation.emailIsConfirmed(req.body.email, req.body["confirm-email"])
+    !validation.emailIsConfirmed(req.body.email, req.body['confirm-email'])
   ) {
     sessionFlash.flashDataToSession(
       req,
       {
         errorMessage:
-          "Please check your input. Password must be at least six characters long, postal code five characters.",
+          'Please check your input. Password must be at least 6 character slong, postal code must be 5 characters long.',
         ...enteredData,
       },
       function () {
-        res.redirect("/signup");
+        res.redirect('/signup');
       }
     );
     return;
@@ -73,22 +73,23 @@ async function signup(req, res, next) {
       sessionFlash.flashDataToSession(
         req,
         {
-          errorMessage: "User exists already! Try logging in instead!",
+          errorMessage: 'User exists already! Try logging in instead!',
           ...enteredData,
         },
         function () {
-          res.redirect("/signup");
+          res.redirect('/signup');
         }
       );
       return;
     }
+
     await user.signup();
-  } catch (e) {
-    next(e);
+  } catch (error) {
+    next(error);
     return;
   }
 
-  res.redirect("/login");
+  res.redirect('/login');
 }
 
 function getLogin(req, res) {
@@ -96,11 +97,12 @@ function getLogin(req, res) {
 
   if (!sessionData) {
     sessionData = {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
     };
   }
-  res.render("customer/auth/login", { inputData: sessionData });
+
+  res.render('customer/auth/login', { inputData: sessionData });
 }
 
 async function login(req, res, next) {
@@ -108,20 +110,21 @@ async function login(req, res, next) {
   let existingUser;
   try {
     existingUser = await user.getUserWithSameEmail();
-  } catch (e) {
-    next(e);
+  } catch (error) {
+    next(error);
     return;
   }
 
   const sessionErrorData = {
-    errorMessage: "Invalid credentials - Please check email and password",
+    errorMessage:
+      'Invalid credentials - please double-check your email and password!',
     email: user.email,
     password: user.password,
   };
 
   if (!existingUser) {
     sessionFlash.flashDataToSession(req, sessionErrorData, function () {
-      res.redirect("/login");
+      res.redirect('/login');
     });
     return;
   }
@@ -132,19 +135,19 @@ async function login(req, res, next) {
 
   if (!passwordIsCorrect) {
     sessionFlash.flashDataToSession(req, sessionErrorData, function () {
-      res.redirect("/login");
+      res.redirect('/login');
     });
     return;
   }
 
   authUtil.createUserSession(req, existingUser, function () {
-    res.redirect("/");
+    res.redirect('/');
   });
 }
 
 function logout(req, res) {
   authUtil.destroyUserAuthSession(req);
-  res.redirect("/login");
+  res.redirect('/login');
 }
 
 module.exports = {
